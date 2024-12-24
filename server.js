4549387c528d4bfd9153042efb51aa02f0732d94
Akshay Rajmane <akshay.rajmane93@gmail.com>
2024-12-24 19:17:40 +0530

    tt

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
 const axios = require('axios');
+require('dotenv').config();  // Load environment variables from .env
+
 const app = express();
 const port = 5000;
 
-// Replace with your ThingSpeak Read API Key and Channel ID
-const thingSpeakReadAPIKey = 'PYLM6TN4B3HFDCON';
-const channelID = '2794295';
+const thingSpeakReadAPIKey = process.env.THINGSPEAK_READ_API_KEY;
+const channelID = process.env.THINGSPEAK_CHANNEL_ID;
 
 app.get('/get-temperature', async (req, res) => {
   try {
     const response = await axios.get(`https://api.thingspeak.com/channels/${channelID}/fields/1.json?api_key=${thingSpeakReadAPIKey}&results=1`);
-    const temperatureData = response.data.feeds[0].field1;  // Access the latest temperature data
+    const temperatureData = response.data.feeds[0].field1;  // Get the temperature
     res.json({ temperature: temperatureData });
   } catch (error) {
     console.error('Error fetching temperature from ThingSpeak:', error);
@@ -18,7 +19,6 @@ app.get('/get-temperature', async (req, res) => {
   }
 });
 
-// Start server
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
