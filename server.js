d589a67ae5b21849fe641b0d570eddefc22ae58c
Akshay Rajmane <akshay.rajmane93@gmail.com>
2024-12-25 13:23:15 +0530

    qwer

--- server.js
+++ server.js
@@ -11,9 +11,9 @@ app.use(cors());
 const fetchUrl = process.env.LOCAL_ID;
 
 // Route to fetch temperature from ESP32
-app.get('/get-temperature', async (req, res) => {
+app.get('/temperature', async (req, res) => {
   try {
-    const esp32_ip = `${fetchUrl}/temperature`; // Replace with your ESP32 local IP
+    const esp32_ip = fetchUrl; // Replace with your ESP32 local IP
     const response = await axios.get(esp32_ip);
     res.json(response.data); // Send the data received from ESP32 to the client
   } catch (error) {
